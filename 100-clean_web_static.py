a9f1bb7e49e3298d8fd9722870d5f02a8ff466b1
hunterxx0 <azouzimhamed@gmail.com>
2020-08-17 23:12:54 +0000

    first

---
+++ 100-clean_web_static.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Fabric script that generates a .tgz archive.
+"""
+from datetime import datetime
+from fabric.api import *
+from os import walk
+env.hosts = ['35.231.7.140', '35.227.72.212']
+
+
+def do_clean(number=0):
+    if int(number) == 0:
+        number = 1
+    else:
+        number = int(number)
+    f = []
+    for (dirpath, dirnames, filenames) in walk("./versions/"):
+        f.extend(filenames)
+        break
+    f.sort()
+    for i in range(number):
+        f.pop()
+    with lcd("./versions/"):
+        for i in f:
+                local("rm -rf {}".format(i))
+    with cd("/data/web_static/releases"):
+        h = run("ls -rt").split()
+        z = [i for i in h if "_static_" in i]
+        for i in range(number):
+            z.pop()
+        for i in z:
+            local("rm -rf {}".format(i))
---
+++ 101-setup_web_static.pp
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# configure web-02 to be identical to web-01.
+exec {'setup':
+command  => 'sudo apt-get update -y; sudo apt-get install nginx -y; sudo mkdir -p /data/web_static/releases/test/; sudo mkdir -p /data/web_static/shared/; sudo touch /data/web_static/releases/test/index.html; sudo chown -R ubuntu:ubuntu /data/; sudo echo "Hello world!" > /data/web_static/releases/test/index.html; sudo ln -sf /data/web_static/releases/test /data/web_static/current; sudo chown -R ubuntu:ubuntu /data/; sudo sed -i '37i\\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default ; sudo service nginx restart',
+provider => shell,
+}
