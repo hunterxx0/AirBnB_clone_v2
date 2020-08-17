22181b4338b6efa1be8766db947e92a492dba9bb
hunterxx0 <azouzimhamed@gmail.com>
2020-08-17 23:31:21 +0000

    first

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # configure web-02 to be identical to web-01.
 exec {'setup':
-command  => 'apt-get update -y; apt-get install nginx -y; mkdir -p /data/web_static/shared/ /data/web_static/releases/test/; touch /data/web_static/releases/test/index.html; chown -R ubuntu:ubuntu /data; echo "Hello World!" > /data/web_static/releases/test/index.html; ln -sf /data/web_static/releases/test/ /data/web_static/current; chown -R ubuntu:ubuntu /data; sed -i '37i\\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default; sudo service nginx restart;
+command  => 'apt-get update -y; apt-get install nginx -y; mkdir -p /data/web_static/shared/ /data/web_static/releases/test/; touch /data/web_static/releases/test/index.html; chown -R ubuntu:ubuntu /data; echo "Hello World!" > /data/web_static/releases/test/index.html; ln -sf /data/web_static/releases/test/ /data/web_static/current; chown -R ubuntu:ubuntu /data; sed -i '37i\\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default; service nginx restart;
 ',
 provider => shell,
 }
