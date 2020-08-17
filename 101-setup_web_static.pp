a73f040930356d068b17f6deb4db67a4f79468ec
hunterxx0 <azouzimhamed@gmail.com>
2020-08-17 23:32:43 +0000

    first

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,5 +1,4 @@
-#!/usr/bin/env bash
-# configure web-02 to be identical to web-01.
+# Puppet for setup
 exec {'setup':
 command  => 'apt-get update -y; apt-get install nginx -y; mkdir -p /data/web_static/shared/ /data/web_static/releases/test/; touch /data/web_static/releases/test/index.html; chown -R ubuntu:ubuntu /data; echo "Hello World!" > /data/web_static/releases/test/index.html; ln -sf /data/web_static/releases/test/ /data/web_static/current; chown -R ubuntu:ubuntu /data; sed -i '37i\\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default; service nginx restart;
 ',
